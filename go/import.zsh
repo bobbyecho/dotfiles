0b7c564f34199e7191e972fcc8e32c8767abe34c
Bobby Echo Prasetyo <bobbyechop@gmail.com>
2023-03-09 21:46:09 +0700

    feat: add go language

---
+++ go/import.zsh
@@ -0,0 +1,5 @@
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=$HOME/code/go
+[ -d $GOPATH/src ] || mkdir -p $GOPATH/src
+[ -d $GOPATH/pkg ] || mkdir -p $GOPATH/pkg
+[ -d $GOPATH/bin ] || mkdir -p $GOPATH/bin
